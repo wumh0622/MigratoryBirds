a2cd6d62afd69cf90f80e55837cae703e47c3ac6
吳孟宏 <wueric0622@gmail.com>
2018-01-27 00:35:28 +0800

    Add 背景scrolling

---
+++ Assets/Scripts/Object/BackGroundScrollDown.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundScrollDown : MonoBehaviour {
+
+    public static float speed;
+
+    // Use this for initialization
+    void Start () {
+		Camera mainCamera = Camera.main;
+		transform.localScale = new Vector3(2f * mainCamera.orthographicSize * mainCamera.aspect, 2f * mainCamera.orthographicSize, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void LateUpdate()
+	{
+		Camera mainCamera = Camera.main;
+		if(transform.position.y <= -2f * mainCamera.orthographicSize)
+		{
+            transform.position =  transform.position + new Vector3(0, 6f * mainCamera.orthographicSize, 0);
+        }
+        transform.Translate(0, speed*-1, 0);
+    }
+}
---
+++ Assets/Scripts/System/LevelGenerate.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerate : MonoBehaviour {
+
+	public static LevelGenerate instence;
+
+	void Awake()
+	{
+		if(instence == null)
+		{
+            instence = this;
+        }
+		else
+		{
+            Destroy(gameObject);
+        }
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//背景圖片生成
+    [SerializeField] GameObject backGround;
+    [SerializeField] float scrollingSpeed;
+
+    public void GenerateBackGround()
+	{
+        BackGroundScrollDown.speed = scrollingSpeed;
+        for (int i = 1; i <= 2; i++)
+        {
+            Camera mainCamera = Camera.main;
+            GameObject clone = Instantiate(backGround);
+            clone.transform.position = new Vector3(0, 2f * i * mainCamera.orthographicSize, 0);
+            clone.transform.localScale = new Vector3(2f * mainCamera.orthographicSize * mainCamera.aspect, 2f * mainCamera.orthographicSize, 0);
+        }
+    }
+
+    //島嶼生成
+
+    [SerializeField] GameObject[] Island;
+
+    public void GenerateIsland()
+	{
+		
+	} 
+}
