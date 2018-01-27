7a53b8650b6328a03555e86d28d9f0b3374548b8
Jared <QiFen.Pocket@gmail.com>
2018-01-27 16:00:46 +0800

    Implement Enemy

---
+++ Assets/Scripts/Object/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public int numberOfBirds = 100;
+    public int sp = 100;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void killBirds(int birds)
+    {
+        numberOfBirds -= birds;
+        //birds die animation
+    }
+
+    public void hurtSP(int numberOfSP)
+    {
+        sp -= numberOfSP;
+    }
+
+    public void attack(GameObject enemy)
+    {
+        //call birds attack
+    }
+}
