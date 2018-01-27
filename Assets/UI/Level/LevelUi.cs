04c5f5638c756d2e8a7ad47707d58ad97518e6e8
gmfnasg <gpx_mark_farng@hotamil.com>
2018-01-27 18:57:21 +0800

    主畫面與關卡ui

---
+++ Assets/UI/Level/LevelUi.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelUi : MonoBehaviour {
+	public Image HpBarFull;
+
+	public Button Pause;
+	public Button Resume;
+
+	public Button MainButton;
+	public CanvasGroup GameOverCanvasGroup;
+
+	public string MainSceneNmae;
+
+	private void Awake()
+	{
+		Pause.gameObject.SetActive(false);
+		Resume.gameObject.SetActive(false);
+		GameOverCanvasGroup.alpha = 0;
+	}
+
+	private void Update()
+	{
+		float rate = GameManager.Instance.sp / 100;
+		HpBarFull.fillAmount = Mathf.Lerp(0, 1, rate);
+	}
+
+	public void GoToMainScene()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene("Start");
+	}
+}
---
+++ Assets/UI/Start/MainScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScene : MonoBehaviour {
+	public string GameSceneName = "MainLevel";
+	public void GameStart()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene(GameSceneName);
+	}
+	public void End()
+	{
+		Application.Quit();
+	}
+}
